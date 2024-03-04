cb17a7d473196ae8d87b2577a7db1cffb3584c38
Steve Ryan <steve.ryan@asu.edu>
2024-03-04 16:07:34 -0700

    Remove unused .js and PHP files

--- dist/js/child-theme.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '3c9bd99992337eac8ea1');
--- src/scripts/child-theme.js
+++
@@ -1,6 +0,0 @@
-// Start your project's JS here.
-// If you are rolling some jQuery you'll want to use a doc ready statement like the following.
-
-jQuery(document).ready(function($){
-    // Code is poetry.
-});
